8126234d4f0dc07e74a1febe15e0253af247addc
Lê Trung Kiên <144931082+kudoshinichi2k5@users.noreply.github.com>
2025-12-18 08:28:37 +0700

    feat(infra): update EKS node group instance type and scaling config (#16)

--- 2-terraform-eks-deployment/modules/eks/main.tf
+++ 2-terraform-eks-deployment/modules/eks/main.tf
@@ -67,12 +67,12 @@ resource "aws_eks_node_group" "this" {
   subnet_ids     = var.subnet_ids
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     min_size     = 1
-    max_size     = 1
+    max_size     = 3
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.medium"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_policies
